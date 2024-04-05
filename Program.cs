b573774dd57faab52b6b57c74d23d22fd08666ea
Kethytektek <163605882+Kethytektek@users.noreply.github.com>
2024-04-05 16:09:09 -0300

    dadinho

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography;
+
+Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+Console.WriteLine("Rolagem de Dado.");
+Console.WriteLine();
+
+Console.Write("Quantas faces vai ter o dado? ");
+
+int faces = Convert.ToInt32(Console.Read());
+int rolar = RandomNumberGenerator.GetInt32(1, faces);
+Console.WriteLine();
+
+Console.WriteLine($"A rolagem do dado resultou em {rolar}");
+Console.WriteLine("-----------------------------------------------------------------");
